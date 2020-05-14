f053ec2c2e7998c6edfb88dc66ecd5aba5031f26
Victor Hideo Oka <oka.victor97@gmail.com>
2020-05-14 20:47:25 -0300

    add work page custom widgets

---
+++ lib/screens/onboarding/pages/work/work_text_column.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_concept/screens/onboarding/widgets/text_column.dart';
+
+class WorkTextColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextColumn(
+      title: 'Work together',
+      text: 'Some cool text about the work page goes here, \nas part of the layout!',
+    );
+  }
+}
