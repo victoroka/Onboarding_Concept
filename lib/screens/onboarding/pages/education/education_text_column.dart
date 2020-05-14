ac6a0383a6fce5bd25c87fa73f21d89a567ffb9e
Victor Hideo Oka <oka.victor97@gmail.com>
2020-05-14 20:34:04 -0300

    add education page custom widgets

---
+++ lib/screens/onboarding/pages/education/education_text_column.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_concept/screens/onboarding/widgets/text_column.dart';
+
+class EducationTextColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextColumn(
+      title: 'Keep learning',
+      text: 'Some cool text about the education page goes here, \nas part of the layout!',
+    );
+  }
+}
